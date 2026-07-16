82a7a76b0bee9d87d6cabda868b799a8acb64440
jrciocca <jrciocca@gmail.com>
2026-07-15 22:39:58 -0300

    Espaco de seguranca embaixo da modal e fonte maior

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'insens-suinos-v8';
+const CACHE_NAME = 'insens-suinos-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
