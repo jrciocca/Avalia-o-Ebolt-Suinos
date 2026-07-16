278b9da3ca1f3d55ce7d086250d34339414852f8
jrciocca <jrciocca@gmail.com>
2026-07-15 22:13:58 -0300

    Aumenta ainda mais fontes/botoes e sobe versao do cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'insens-suinos-v2';
+const CACHE_NAME = 'insens-suinos-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
