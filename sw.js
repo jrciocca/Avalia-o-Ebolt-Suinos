a3adc313ea67770873081d7a2eb7f6133ab57963
jrciocca <jrciocca@gmail.com>
2026-07-15 22:37:24 -0300

    Aumenta fonte da modal de falha (chips e botoes)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'insens-suinos-v7';
+const CACHE_NAME = 'insens-suinos-v8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
